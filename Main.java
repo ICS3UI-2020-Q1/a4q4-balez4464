f0a6965380f99857f94c16d1c6e2af0e8a40f2e3
balez4464 <71140813+balez4464@users.noreply.github.com>
2020-10-02 11:33:33 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ *This program asks user for their number and give them their fizz buzz seqeunce
+ * @author Zachary Balean
  */
 public class Main {
 
@@ -9,7 +11,34 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // creates Scanner
+    Scanner input = new Scanner(System.in);
+
+    //asks user for a number
+    System.out.println("Please enter a positive integer to see the FizzBuzz numbers");
+
+    //users number
+    int usersNumber = input.nextInt();
+    //number for seqeunce
+    int countUp = 1;
+
+    //while loop for fizzbuzz seqeunce
+    while(countUp <= usersNumber){
+      if(countUp %3 == 0 && countUp %5 == 0){
+        System.out.println("FizzBuzz");
+      }else if(countUp %3 == 0){
+        System.out.println("Fizz");
+      } else if(countUp %5 == 0){
+        System.out.println("Buzz"); 
+      } else{
+        System.out.println(countUp);
+      }
+      countUp = countUp + 1;
+    }
+
+      
+
     
-  }
+  }  
+  
 }
